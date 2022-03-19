0b354e167b29c07adb32bf62a0f92b228461c875
Yaman KATBY <m.yaman.katby@gmail.com>
2022-03-19 11:48:11 +0300

    Work on the Main Layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,66 @@
 import { Outlet, Navigate } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { useIsLoggedIn } from "../config/hooks";
+import { useState } from "react";
 
 export default function Layout() {
   const isLoggedIn = useIsLoggedIn();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
   if (isLoggedIn === null) return <h1>Loading...</h1>;
   else if (isLoggedIn === false) return <Navigate replace to="/sign-in" />;
 
-  return <Outlet />;
+  return (
+    <>
+      <AppBar position="apsolute">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Home
+          </Typography>
+          <IconButton
+            color="inherit"
+            size="large"
+            onClick={(e) => {
+              setAnchorEl(e.currentTarget);
+            }}
+          >
+            <AccountCircleIcon />
+          </IconButton>
+
+          <Menu
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={() => {
+              setAnchorEl(null);
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                setAnchorEl(null);
+              }}
+            >
+              Profile
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setAnchorEl(null);
+              }}
+            >
+              Sign out
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </>
+  );
 }
