71b7e4b843b266d18d7b91eba20bf8ac0bfe4c01
Yaman KATBY <m.yaman.katby@gmail.com>
2022-03-21 10:32:48 +0300

    Work on the Main Layout menu buttons

--- src/config/hooks.js
+++ src/config/hooks.js
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
 
+export const useCurrentUser = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  return user;
+};
+
 export const useIsLoggedIn = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(null);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      console.log(user);
+    return onAuthStateChanged(auth, (user) => {
       setIsLoggedIn(!!user);
     });
   }, []);
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,15 +6,32 @@ import {
   IconButton,
   Menu,
   MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Avatar,
+  Box,
+  DialogActions,
+  Button,
+  DialogContentText,
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { useIsLoggedIn } from "../config/hooks";
+import { useIsLoggedIn, useCurrentUser } from "../config/hooks";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logOut } from "../redux/authSlice";
 
 export default function Layout() {
+  const currentUser = useCurrentUser();
   const isLoggedIn = useIsLoggedIn();
 
+  const dispatch = useDispatch();
+
   const [anchorEl, setAnchorEl] = useState(null);
+
+  const [profileDialogOpen, setProfileDialogOpen] = useState(false);
+  const [confirmSignOutDialogOpen, setConfirmSignOutDialogOpen] =
+    useState(false);
 
   if (isLoggedIn === null) return <h1>Loading...</h1>;
   else if (isLoggedIn === false) return <Navigate replace to="/sign-in" />;
@@ -46,6 +63,7 @@ export default function Layout() {
             <MenuItem
               onClick={() => {
                 setAnchorEl(null);
+                setProfileDialogOpen(true);
               }}
             >
               Profile
@@ -53,6 +71,7 @@ export default function Layout() {
             <MenuItem
               onClick={() => {
                 setAnchorEl(null);
+                setConfirmSignOutDialogOpen(true);
               }}
             >
               Sign out
@@ -60,6 +79,64 @@ export default function Layout() {
           </Menu>
         </Toolbar>
       </AppBar>
+
+      <Dialog
+        open={profileDialogOpen}
+        onClose={() => {
+          setProfileDialogOpen(false);
+        }}
+      >
+        <DialogTitle>Profile</DialogTitle>
+        <DialogContent dividers>
+          <Box display="flex" alignItems="center">
+            <Avatar />
+            <Box ml={3}>
+              <Typography>{currentUser?.displayName}</Typography>
+              <Typography>{currentUser?.email}</Typography>
+            </Box>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setProfileDialogOpen(false);
+            }}
+          >
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={confirmSignOutDialogOpen}
+        onClose={() => {
+          setConfirmSignOutDialogOpen(false);
+        }}
+      >
+        <DialogContent>
+          <DialogContentText>
+            Sign out from "{currentUser?.email}"
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              setConfirmSignOutDialogOpen(false);
+              dispatch(logOut());
+            }}
+          >
+            Sign out
+          </Button>
+          <Button
+            onClick={() => {
+              setConfirmSignOutDialogOpen(false);
+            }}
+          >
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <Outlet />
     </>
   );
